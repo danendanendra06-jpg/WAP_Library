4723b311c7cb2abaab0b012b8dae6b33f2c6d979
Danendra <danendanendra06@gmail.com>
2025-11-30 19:34:01 +0700

    Update Dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,3 @@
-# Gunakan image PHP 8.1 FPM resmi (yang berbasis Debian)
 FROM php:8.1-fpm
-
-# Instal ekstensi mysqli yang diperlukan oleh aplikasi Anda
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+
